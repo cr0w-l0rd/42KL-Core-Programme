2b1cf288601b3a08da499479426b998f701e389e
May Elisa Biusing <crowlord@Mays-MacBook-Pro.local>
2025-10-25 04:58:50 +0800

    memmove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 16:39:49 by mbiusing          #+#    #+#             */
-/*   Updated: 2025/10/24 16:39:51 by mbiusing         ###   ########.fr       */
+/*   Updated: 2025/10/25 04:49:12 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-    char    *cpydest;
-    char    *cpysrc;
-    char    *temp;
+	char	*dest_cpy;
+	char	*src_cpy;
+	size_t	i;
 
-    cpydest = (char *) dest;
-    cpysrc = (char *) src;
-    temp = 
-    while ()
+	if (!dest && !src)
+		return (NULL);
+	cpydest = (char *) dest;
+	cpysrc = (char *) src;
+	i = 0;
+	if (dest_cpy > src_cpy)
+	{
+		while (len-- > 0)
+			dest_cpy[len] = src_cpy[len];
+	}
+	else
+	{
+		while (i++ < len)
+			dest_cpy[i] = src_cpy[i];
+	}
+	return (dest);
 }
