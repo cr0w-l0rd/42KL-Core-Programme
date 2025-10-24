2f18de5ec516b1889d3d5909880ebbac28a520d1
May Elisa Biusing <crowlord@Mays-MacBook-Pro.local>
2025-10-24 16:34:36 +0800

    added prototype to libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,14 +6,17 @@
 /*   By: crowlord <crowlord@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 01:45:34 by crowlord          #+#    #+#             */
-/*   Updated: 2025/10/24 01:53:32 by crowlord         ###   ########.fr       */
+/*   Updated: 2025/10/24 16:34:10 by crowlord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stddef.h>
-#include <stdlib.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 
 #endif
