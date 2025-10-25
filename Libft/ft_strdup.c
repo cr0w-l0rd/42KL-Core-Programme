7c84c7adfee3754d1d39245e4ba021eed91ed361
May Elisa Biusing <crowlord@Mays-MacBook-Pro.local>
2025-10-25 23:04:37 +0800

    did strdup n strlcat

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 22:56:31 by mbiusing          #+#    #+#             */
+/*   Updated: 2025/10/25 23:03:45 by mbiusing         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strdup(const char *s1)
+{
+	char	*dest;
+	size_t		i;
+	
+	dest = (char *) malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
